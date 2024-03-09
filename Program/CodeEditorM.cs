8d52bf2a4e509157ab69ca97b3a16c985f404cba
Leva Belokon <levabelokon2222@gmail.com>
2024-03-09 20:04:48 +0300

    Реализованы основные методы класса CodeEditor

--- Program/CodeEditorM.cs
+++ Program/CodeEditorM.cs
@@ -9,12 +9,20 @@ namespace ColorProgPad
 
         public CodeEditorForm()
         {
-
+            InitializeComponent();
+            InitializeCodeEditor();
         }
 
         private void InitializeCodeEditor()
         {
+            codeTextBox = new TextBox();
+            codeTextBox.Multiline = true;
+            codeTextBox.ScrollBars = ScrollBars.Both;
+            codeTextBox.Dock = DockStyle.Fill;
 
+            //Необходимо добавить обработчики событий...
+
+            Controls.Add(codeTextBox);
         }
 
 
@@ -22,7 +30,9 @@ namespace ColorProgPad
         [STAThread]
         static void Main()
         {
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CodeEditorForm());
         }
 
     }
