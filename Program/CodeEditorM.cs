08e802241824c447cfe48b92b2e4b739de79f766
Leva Belokon <levabelokon2222@gmail.com>
2024-03-09 20:00:57 +0300

    Объявлены основные методы класса CodeEditor

--- Program/CodeEditorM.cs
+++ Program/CodeEditorM.cs
@@ -5,6 +5,25 @@ namespace ColorProgPad
 {
     public class CodeEditor : Form
     {
+        private TextBox codeTextBox;
+
+        public CodeEditorForm()
+        {
+
+        }
+
+        private void InitializeCodeEditor()
+        {
+
+        }
+
+
+
+        [STAThread]
+        static void Main()
+        {
+
+        }
 
     }
 }
