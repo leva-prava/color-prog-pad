1b767fef009874e1ef5a81c2bc87b21e1b7d543e
Leva Belokon <levabelokon2222@gmail.com>
2024-03-10 12:34:57 +0300

    Выполнена реализация основных методов класса Terminal

--- Program/Terminal.cs
+++ Program/Terminal.cs
@@ -4,7 +4,23 @@ namespace ColorProgPad
 {
     class Terminal
     {
+        public void Run()
+        {
+            Console.WriteLine("Приветствую в терминале! Введите 'help' для получения списка команд.");
 
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                ProcessCommand(input);
+            }
+        }
+
+        private void ProcessCommand(string command)
+        {
+            // Реализация обработки команд
+            Console.WriteLine($"Вы ввели команду: {command}");
+        }
     }
 
 }
