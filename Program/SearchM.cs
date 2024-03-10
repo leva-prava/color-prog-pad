8ea88d05dab6d56869383aa3e21a572a70831287
Leva Belokon <levabelokon2222@gmail.com>
2024-03-10 13:16:16 +0300

    Внесены изменения в класс SearchM. Добавлен метод ReplaceUsingRegex

--- Program/SearchM.cs
+++ Program/SearchM.cs
@@ -6,13 +6,22 @@ namespace ColorProgPad
     {
         public string Search(string code, string searchTerm)
         {
+            StringComparison comparison = caseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
             //Поиск строки в коде
             return code.Contains(searchTerm) ? $"Найдено: {searchTerm}" : $"Не найдено: {searchTerm}";
         }
 
         public string Replace(string code, string searchTerm, string replacement)
         {
+            StringComparison comparison = caseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+
             string replacedCode = code.Replace(searchTerm, replacement);
+            return replacedCode;
+        }
+
+        public string ReplaceUsingRegex(string code, string pattern, string replacement)
+        {
+            string replacedCode = Regex.Replace(code, pattern, replacement);
             return replacedCode;
         }
     }
