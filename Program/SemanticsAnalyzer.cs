7cddf6de919b17339fac848e63b618d8f3778212
Leva Belokon <levabelokon2222@gmail.com>
2024-03-10 12:56:22 +0300

    Объвлены основные свойства и методы класса SemanticsAnalyzer

--- Program/SemanticsAnalyzer.cs
+++ Program/SemanticsAnalyzer.cs
@@ -5,6 +5,22 @@ namespace ColorProgPad
 {
     class SemanticsAnalyzer
     {
+        private List<string> functions = new List<string>();
+        private List<string> variables = new List<string>();
 
+        public void AnalyzeCode(string code)
+        {
+
+        }
+
+        public List<string> GetFunctions()
+        {
+
+        }
+
+        public List<string> GetVariables()
+        {
+            
+        }
     }
 }
