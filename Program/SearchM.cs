009d5084b9cc27323ec3dfae5370cbd63f55f982
Leva Belokon <levabelokon2222@gmail.com>
2024-03-10 13:13:41 +0300

    Реализованы основные методы класса SearchM

--- Program/SearchM.cs
+++ Program/SearchM.cs
@@ -6,12 +6,14 @@ namespace ColorProgPad
     {
         public string Search(string code, string searchTerm)
         {
-
+            //Поиск строки в коде
+            return code.Contains(searchTerm) ? $"Найдено: {searchTerm}" : $"Не найдено: {searchTerm}";
         }
 
         public string Replace(string code, string searchTerm, string replacement)
         {
-            
+            string replacedCode = code.Replace(searchTerm, replacement);
+            return replacedCode;
         }
     }
 }
