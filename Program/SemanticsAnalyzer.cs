d36f8333f560157cc1f12c7ce421ebe182c0912f
Leva Belokon <levabelokon2222@gmail.com>
2024-03-10 13:01:10 +0300

    Выполнена реализация основных методов класса SemanticsAnalyzer

--- Program/SemanticsAnalyzer.cs
+++ Program/SemanticsAnalyzer.cs
@@ -10,17 +10,28 @@ namespace ColorProgPad
 
         public void AnalyzeCode(string code)
         {
+             if (code.Contains("function"))
+            {
+                //пример добавления функции анализатором
+                functions.Add("add");
+            }
 
+            if (code.Contains("var"))
+            {
+                // пример добавления переменной анализатором
+                variables.Add("a");
+                variables.Add("b");
+            }
         }
 
         public List<string> GetFunctions()
         {
-
+            return functions;
         }
 
         public List<string> GetVariables()
         {
-            
+            return variables;
         }
     }
 }
